99a236811cf670a2fc783e0963c80ee3e377eddd
BlakeWiscombe <2011264@schoolsnet.act.edu.au>
2022-05-20 15:47:04 +1000

    Added up and down movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -27,6 +27,10 @@ func _physics_process(delta):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_right"):
 		move_and_collide(Vector2(movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(0, -movement_speed * delta))
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(0, movement_speed * delta))
 	#if Input.is_action_pressed("ui_up"):
 	#	get_tree().change_scene("res://WinScene.tscn")
 			
