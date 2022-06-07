e903ad741f1b9187e662edfd9e55650a2eb4b34a
Blake <2011264@schoolsnet.act.edu.au>
2022-06-07 14:32:22 +1000

    cheat screen

---
+++ Cheat screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_return_pressed():
+	get_tree().change_scene("res://MainGame/MainGame.tscn")
---
+++ back to main menu.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+
+
+
+func _on_return_pressed():
+	get_tree().change_scene("res://menu/Menu.tscn")
